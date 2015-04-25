4266051f361f4556d89ff2d4bb08b5ae6bef0e6a
Scott Prue <sprue.dev@gmail.com>
2015-04-24 20:45:56 -0400

    Query parameter names and order modified to match API documentation for initial auth request.

--- machines/spotify-login-url.js
+++ machines/spotify-login-url.js
@@ -1,36 +1,42 @@
 module.exports = {
 
   friendlyName: 'Get login URL',
-  description: 'Get the URL on Spotify that a user should visit to authorize the specified SoundCloud app (i.e. your app).',
+  description: 'Get the URL on Spotify that a user should visit to authorize the specified Spotify app (i.e. your app).',
+
   extendedDescription: 'This is the URL where you typically redirect a user in order for them to grant access to your Spotify app.',
+
   cacheable: true,
 
   inputs: {
     clientId: {
       example: 'abc123jdhs3h4js',
-      description: 'The unique identifier for your Spotify app. It\'s listed on the application settings page.',
+      description: 'The clientId for your Spotify app. The client ID provided to you by Spotify when you register your application.',
       required: true
     },
-    callbackUrl: {
+    redirectUri: {
       example: 'https://example.com/callback?code=NApCCg..BkWtQ&state=profile%2Factivity',
-      description: 'The URL in your app where users will be sent after authorization.',
+      description: 'The URI to redirect to after the user grants/denies permission. This URI needs to have been entered in the Redirect URI whitelist that you specified when you registered your application.',
       required: true
     },
     responseType: {
       example: ['code'],
       description: '(code, token_and_code)'
     },
-    scope: {
-      example: 'non-expiring',
-      description: 'Must be `*` or `non-expiring`.'
+    state:{
+      description:'The state can be useful for correlating requests and responses. Because your redirect_uri can be guessed, using a state value can increase your assurance that an incoming connection is the result of an authentication request. If you generate a random string or encode the hash of some client state (e.g., a cookie) in this state variable, you can validate the response to additionally ensure that the request and response originated in the same browser.'
     },
-    display: {
-      example: 'popup',
-      description: 'Can specify a value of `popup` for mobile optimized screen'
+    scope: {
+      example: '*',
+      description: 'A space-separated list of scopes: see Using Scopes. If no scopes are specified, authorization will be granted only to access publicly available information: that is, only information normally visible in the Spotify desktop, web and mobile players. '
+    },
+    dialog: {
+      example: 'false',
+      description: 'Whether or not to force the user to approve the app again if they’ve already done so. If false (default), a user who has already approved the application may be automatically redirected to the URI specified by redirect_uri. If true, the user will not be automatically redirected and will have to approve the app again.'
     }
   },
 
   defaultExit: 'success',
+
   catchallExit: 'error',
 
   exits: {
@@ -53,8 +59,8 @@ module.exports = {
 
     try {
       return exits.success(util.format(
-        'https://accounts.spotify.com/authorize',
-        inputs.clientId, inputs.callbackUrl, inputs.responseType, inputs.scope, state
+        'https://accounts.spotify.com/authorize/?client_id=%s&response_type=%s&redirect_uri=%s&scope=%s&state=%s',
+        inputs.clientId, inputs.responseType, inputs.redirectUri, inputs.scope, state
       ));
     }
     catch(e) {
